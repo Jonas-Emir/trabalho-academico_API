672dd703685fee5bd007a3fa06ab5c5c788d18e7
Jonas Emir <jonasemir00@gmail.com>
2024-11-18 23:58:01 -0300

    Prévia da implementação para o Crud de Categoria, criação da controller, repositorio, mapper, dto e ajuste na estrutura de pastas

---
+++ GestaoEstoque_API/Infrastructure/Repositories/Interface/ICategoriaRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using GestaoEstoque_API.Application.Dtos;
+using GestaoEstoque_API.Domain.Entities;
+
+namespace GestaoEstoque_API.Infrastructure.Repositories.Interface
+{
+    public interface ICategoriaRepositorio
+    {
+        List<CategoriaResponseDto> BuscarCategorias();
+        CategoriaResponseDto BuscarPorId(int categoriaId);
+        Task<RequestCategoriaDto> Adicionar(RequestCategoriaDto categoriaDto);
+        Task<RequestCategoriaDto> Atualizar(RequestCategoriaDto categoriaDto, int categoriaId);
+        bool Apagar(int categoriaId);
+    }
+}
