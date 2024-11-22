2b0e130163692e4c0cd971cee2762725b7f6ac3b
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2024-11-21 23:44:58 -0300

    Serviços HTTP para Estoques

--- GestaoEstoque_API/Controllers/EstoqueController.cs
+++ GestaoEstoque_API/Controllers/EstoqueController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GestaoEstoque_API.Application.Dtos.Movimentacao;
+using GestaoEstoque_API.Infrastructure.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GestaoEstoque_API.Controllers
 {
-    public class EstoqueController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstoqueController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IEstoqueRepositorio _estoqueRepositorio;
+
+        public EstoqueController(IEstoqueRepositorio estoqueRepositorio)
         {
-            return View();
+            _estoqueRepositorio = estoqueRepositorio;
+        }
+
+        [HttpGet("ListarEstoques")]
+        public async Task<ActionResult<List<MovimentacaoEstoqueDto>>> BuscarTodosEstoques()
+        {
+            var estoques = await _estoqueRepositorio.BuscarEstoques();
+            return Ok(estoques);
+        }
+
+        [HttpGet("BuscarPorId/{id}")]
+        public async Task<ActionResult<MovimentacaoEstoqueDto>> BuscarPorId(int id)
+        {
+            var estoque = await _estoqueRepositorio.BuscarPorId(id);
+
+            if (estoque == null)
+                return NotFound($"Estoque com ID {id} não encontrado.");
+
+            return Ok(estoque);
+        }
+
+        [HttpPost("InserirEstoque")]
+        public async Task<ActionResult<MovimentacaoEstoqueDto>> Cadastrar([FromBody] MovimentacaoEstoqueDto estoque)
+        {
+            var estoqueCadastrado = await _estoqueRepositorio.Adicionar(estoque);
+
+            return Ok(estoqueCadastrado);
+        }
+
+        [HttpPut("AtualizarEstoque/{id}")]
+        public async Task<ActionResult<MovimentacaoEstoqueDto>> Atualizar([FromBody] MovimentacaoEstoqueDto estoqueDto, int id)
+        {
+            var estoqueExistente = await _estoqueRepositorio.BuscarPorId(id);
+
+            if (estoqueExistente == null)
+                return NotFound($"Estoque com ID {id} não encontrado.");
+
+            var estoqueAtualizado = await _estoqueRepositorio.Atualizar(estoqueDto, id);
+            return Ok(estoqueAtualizado);
+        }
+
+        [HttpDelete("ApagarEstoque/{id}")]
+        public async Task<ActionResult<bool>> Apagar(int id)
+        {
+            bool apagado = await _estoqueRepositorio.Apagar(id);
+            if (!apagado)
+                return NotFound($"Estoque com ID {id} não encontrado.");
+
+            return Ok(apagado);
         }
     }
 }
